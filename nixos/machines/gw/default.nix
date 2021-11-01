af96d447b722d0eb2021a1305ef54fe4ed925527
Pavel Borzenkov <pavel@borzenkov.net>
2021-11-01 09:09:26 +0100

    gw: use port forwarding instead of nginx

--- nixos/machines/gw/default.nix
+++ nixos/machines/gw/default.nix
@@ -12,22 +12,50 @@
       (modulesPath + "/profiles/headless.nix")
 
       ../../openssh.nix
-
-      ./valheim.nix
-      ./webapps.nix
     ];
 
-  boot.loader.grub = {
-    enable = true;
-    version = 2;
-    devices = [ "/dev/vda" ];
+  boot = {
+    loader.grub = {
+      enable = true;
+      version = 2;
+      devices = [ "/dev/vda" ];
+    };
+
+    kernel.sysctl = {
+      "net.ipv4.ip_forward" = true;
+    };
   };
 
   networking = {
-    firewall.enable = true;
     hostName = "gw";
-
     interfaces.enp1s0.useDHCP = true;
+
+    firewall =
+      let
+        rock = "100.115.192.117";
+        iface = "enp1s0";
+        valheimCmd = "-i ${iface} -p udp --match multiport --destination-ports 2456,2457 -j DNAT --to-destination ${rock}";
+        nginxCmd = "-i ${iface} -p tcp --match multiport --destination-ports 80,443 -j DNAT --to-destination ${rock}";
+      in
+      {
+        enable = true;
+
+        # Valheim
+        allowedUDPPorts = [ 2456 2457 ];
+        # Nginx
+        allowedTCPPorts = [ 80 443 ];
+
+        extraCommands = ''
+          iptables -t nat -I POSTROUTING -o tailscale0 -j MASQUERADE
+          iptables -t nat -I PREROUTING ${valheimCmd}
+          iptables -t nat -I PREROUTING ${nginxCmd}
+        '';
+        extraStopCommands = ''
+          iptables -t nat -D PREROUTING ${nginxCmd} || true
+          iptables -t nat -D PREROUTING ${valheimCmd} || true
+          iptables -t nat -D POSTROUTING -o tailscale0 -j MASQUERADE || true
+        '';
+      };
   };
 
   sops.defaultSopsFile = ./secrets/secrets.yaml;
