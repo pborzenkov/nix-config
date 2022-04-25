db773f101db7cd3cd39d0358888ba96821fe693f
Pavel Borzenkov <pavel@borzenkov.net>
2022-04-25 21:45:28 +0200

    Update flakes

--- overlay.nix
+++ overlay.nix
@@ -1,22 +1,3 @@
 final: prev:
 
-{
-  calibre = prev.calibre.override {
-    python3Packages = prev.python3Packages.overrideScope (self: super: {
-      # remove after #168071 is merged
-      apsw = super.apsw.overridePythonAttrs (oldAttrs: {
-        version = "3.38.1-r1";
-        sha256 = "sha256-pbb6wCu1T1mPlgoydB1Y1AKv+kToGkdVUjiom2vTqf4=";
-        checkInputs = [ ];
-        checkPhase = ''
-          python tests.py
-        '';
-      });
-    });
-  };
-  jellyfin-mpv-shim = prev.jellyfin-mpv-shim.override {
-    pywebview = prev.python3Packages.pywebview.overrideAttrs (oldAttrs: {
-      doInstallCheck = false;
-    });
-  };
-}
+{ }
