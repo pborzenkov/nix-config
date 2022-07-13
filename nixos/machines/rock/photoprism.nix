777e7cb9163e88c00bdde2d4a96922cc09c8ca73
Pavel Borzenkov <pavel@borzenkov.net>
2022-07-13 22:34:49 +0200

    rock: update photoprism

--- nixos/machines/rock/photoprism.nix
+++ nixos/machines/rock/photoprism.nix
@@ -19,7 +19,7 @@ in
   };
 
   virtualisation.oci-containers.containers.photoprism = {
-    image = "photoprism/photoprism:220617-bullseye";
+    image = "photoprism/photoprism:220629-bullseye";
     ports = [
       "${port}:${port}"
     ];
