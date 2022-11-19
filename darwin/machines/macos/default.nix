65690f13584c7d85c46c03567f0d5ea43889cbbc
Pavel Borzenkov <pavel@borzenkov.net>
2022-11-19 14:38:34 +0100

    macos: allow to login with default ssh key

--- darwin/machines/macos/default.nix
+++ darwin/machines/macos/default.nix
@@ -1,2 +1,6 @@
 { ... }:
-{ }
+{
+  environment.etc."per-user/pbor/authorized_keys".text = ''
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDTeiZ5C7zFAkxo5HGMZfxdAbDtLp6Ktv304NwrhKvkl cardno:000614470090
+  '';
+}
