980f258370fbe06f38fea4906f3c39d93899763d
Pavel Borzenkov <pavel@borzenkov.net>
2022-12-06 16:44:22 +0100

    Allow nixbuild from metal to use macos

--- darwin/machines/macos/default.nix
+++ darwin/machines/macos/default.nix
@@ -3,6 +3,7 @@
   environment = {
     etc."per-user/pbor/ssh/authorized_keys".text = ''
       ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDTeiZ5C7zFAkxo5HGMZfxdAbDtLp6Ktv304NwrhKvkl cardno:000614470090
+      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPwYOwiGiFPur4jIdpndldP5R3GziVmN8v6IC+68NKZe root@metal
     '';
     variables.TERMINFO_DIRS = lib.mkForce "";
   };
