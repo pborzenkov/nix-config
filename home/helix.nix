ffb95787c915db1f9c2a43528fba39ceda1ad94b
Pavel Borzenkov <pavel@borzenkov.net>
2023-01-27 17:49:47 +0100

    helix: switch to HM module

--- home/helix.nix
+++ home/helix.nix
@@ -4,58 +4,38 @@
   inputs,
   ...
 }: {
-  home.packages = [
-    pkgs.helix
-  ];
+  programs.helix = {
+    enable = true;
 
-  xdg.configFile = {
-    "helix/config.toml" = {
-      source = (pkgs.formats.toml {}).generate "config.toml" {
-        theme = config.scheme.slug;
-        editor = {
-          auto-pairs = false;
-          indent-guides = {
-            render = true;
-          };
-          line-number = "relative";
-          mouse = false;
-          shell = ["${pkgs.zsh}/bin/zsh" "-c"];
-          whitespace = {
-            render = {
-              space = "all";
-              tab = "all";
-            };
-          };
+    settings = {
+      theme = config.scheme.slug;
+      editor = {
+        auto-pairs = false;
+        cursor-shape = {
+          insert = "bar";
         };
+        indent-guides = {
+          render = true;
+        };
+        line-number = "relative";
+        mouse = false;
+        shell = ["${pkgs.zsh}/bin/zsh" "-c"];
       };
-      target = "helix/config.toml";
     };
 
-    "helix/languages.toml" = {
-      source = (pkgs.formats.toml {}).generate "languages.toml" {
-        language = [
-          {
-            name = "cpp";
-            auto-format = true;
-          }
-          {
-            name = "nix";
-            formatter = {
-              command = "alejandra";
-              args = ["-"];
-            };
-            auto-format = true;
-          }
-          {
-            name = "perl";
-            language-server = {command = "pls";};
-          }
-          {
-            name = "java";
-            language-server = {command = "java-language-server";};
-          }
-        ];
-      };
-    };
+    languages = [
+      {
+        name = "cpp";
+        auto-format = true;
+      }
+      {
+        name = "nix";
+        formatter = {
+          command = "alejandra";
+          args = ["-"];
+        };
+        auto-format = true;
+      }
+    ];
   };
 }
