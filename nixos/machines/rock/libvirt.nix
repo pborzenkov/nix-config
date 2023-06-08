44e31f276e619173737ca26f011437ebd25b7439
Pavel Borzenkov <pavel@borzenkov.net>
2023-06-08 19:42:31 +0200

    Drop libvirt network autostart workaround

--- nixos/machines/rock/libvirt.nix
+++ nixos/machines/rock/libvirt.nix
@@ -1,11 +1,7 @@
-{lib, ...}: {
+{...}: {
   virtualisation.libvirtd = {
     enable = true;
     allowedBridges = ["mv-host"];
   };
   security.polkit.enable = true;
-
-  systemd.services.libvirtd-config.script = lib.mkAfter ''
-    rm -f /var/lib/libvirt/qemu/networks/autostart/default.xml
-  '';
 }
