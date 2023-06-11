412ed4f2a49e74b11d401d7c73090423d382d9ba
Pavel Borzenkov <pavel@borzenkov.net>
2023-06-11 16:38:29 +0200

    Update ankisyncd-rs

--- packages/ankisyncd-rs/default.nix
+++ packages/ankisyncd-rs/default.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   fetchurl,
   stdenv,
   autoPatchelfHook,
@@ -7,11 +6,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "ankisyncd-rs";
-  version = "1.1.0";
+  version = "1.1.3";
 
   src = fetchurl {
     url = "https://github.com/ankicommunity/anki-sync-server-rs/releases/download/${version}/ankisyncd_${version}_linux_x64.tar.gz";
-    sha256 = "sha256-srsL7gV0ov1yn/Ix8Kh0vrBXoN9p74Pi8NEVHZeNwiM=";
+    sha256 = "sha256-z5aq1EGSfa4ZjMOLLu6p/6cjcWRTwNptyfDNpQIDALY=";
   };
 
   buildInputs = [openssl stdenv.cc.cc.libgcc or null];
