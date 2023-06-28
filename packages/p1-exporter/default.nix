963ea5937034ef29bc5448c886d4c9b49dd1ed3a
Pavel Borzenkov <pavel@borzenkov.net>
2023-06-28 20:11:03 +0200

    p1-exporter: 0.2.3 -> 0.2.4

--- packages/p1-exporter/default.nix
+++ packages/p1-exporter/default.nix
@@ -5,16 +5,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "p1-exporter";
-  version = "0.2.3";
+  version = "0.2.4";
 
   src = fetchFromGitHub {
     owner = "pborzenkov";
     repo = "p1-exporter";
     rev = "v${version}";
-    sha256 = "sha256-7P+NsHp0HuykR6nmPmvB3n+8LQi2p6b66qQwD77TeJk=";
+    sha256 = "sha256-D8jaQvgCcEhy81Zl4BNh401/sQuG6/Z7E73fXZxbrhU=";
   };
 
-  cargoSha256 = "sha256-MYYdrWCzogNtT+7C0UXOgkK6/NXn1L2eVfJLn7LqYrc=";
+  cargoSha256 = "sha256-p++EIbosvAPqZ4ASW7smsyhJTQFCniJH3LrDmWcnsoA=";
 
   meta = with lib; {
     description = "Prometheus exporter for DSMR reader with serial over TCP";
