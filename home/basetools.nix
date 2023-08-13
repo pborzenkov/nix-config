5051d40917b1c7fcb55ab13f2cf174aa66853f5c
Pavel Borzenkov <pavel@borzenkov.net>
2023-08-13 12:28:29 +0200

    home: enable zoxide

--- home/basetools.nix
+++ home/basetools.nix
@@ -37,6 +37,33 @@
       defaultCommand = "fd --type f";
       fileWidgetCommand = "fd --type f";
     };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = false;
+    };
+    zsh.initExtra = ''
+      eval "$(${pkgs.zoxide}/bin/zoxide init zsh)"
+      function __zoxide_zi() {
+        \builtin local result
+        result="$( \
+          zoxide query -ls -- "$@" \
+          | sk \
+            --delimiter='[^\t\n ][\t\n ]+' \
+            -n2.. \
+            --no-sort \
+            --keep-right \
+            --height='40%' \
+            --layout='reverse' \
+            --exit-0 \
+            --select-1 \
+            --bind='ctrl-z:ignore' \
+            --preview='\command -p ls -F --color=always {2..}' \
+          ;
+        )" \
+        && __zoxide_cd "''${result:7}"
+      }
+    '';
   };
 
   home.packages = [
--- home/zsh.nix
+++ home/zsh.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
