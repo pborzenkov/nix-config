7b6a7045f12d0e552e0286d7d37cedde55db69a9
Pavel Borzenkov <pavel@borzenkov.net>
2024-01-16 18:39:08 +0100

    Add Proton VPN config

--- nixos/machines/rock/vpn.nix
+++ nixos/machines/rock/vpn.nix
@@ -3,6 +3,42 @@
   pkgs,
   ...
 }: {
+  systemd.services."netns@" = {
+    description = "%I network namespace";
+    before = ["network.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      PrivateNetwork = true;
+      ExecStart = "${pkgs.writers.writeDash "netns-up" ''
+        ${pkgs.iproute}/bin/ip netns add $1
+        ${pkgs.utillinux}/bin/umount /var/run/netns/$1
+        ${pkgs.utillinux}/bin/mount --bind /proc/self/ns/net /var/run/netns/$1
+      ''} %I";
+      ExecStop = "${pkgs.iproute}/bin/ip netns del %I";
+      PrivateMounts = false;
+    };
+  };
+
+  networking.wireguard = {
+    enable = true;
+    interfaces = {
+      proton = {
+        interfaceNamespace = "proton";
+        privateKeyFile = config.sops.secrets.protonvpn-amsterdam.path;
+        ips = ["10.2.0.2/32"];
+        peers = [
+          {
+            allowedIPs = ["0.0.0.0/0"];
+            endpoint = "185.107.44.110:51820";
+            publicKey = "YgGdHIXeCQgBc4nXKJ4vct8S0fPqBpTgk4I8gh3uMEg=";
+          }
+        ];
+      };
+    };
+  };
+  systemd.services.wireguard-proton.bindsTo = ["netns@proton.service"];
+
   services.netns.namespaces = {
     amsterdam = {
       sysctl = {
@@ -192,6 +228,7 @@
   };
 
   sops.secrets = {
+    protonvpn-amsterdam = {};
     perfect-privacy-password = {};
     perfect-privacy-openvpn-key = {};
   };
