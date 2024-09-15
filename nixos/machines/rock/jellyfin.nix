6b4d24d1a91ce4f18e922a7687f2ed21f4c53026
Pavel Borzenkov <pavel@borzenkov.net>
2024-09-15 19:57:28 +0200

    Update flakes

--- nixos/machines/rock/jellyfin.nix
+++ nixos/machines/rock/jellyfin.nix
@@ -11,9 +11,21 @@
   webapps.apps.jellyfin = {
     subDomain = "jellyfin";
     proxyTo = "http://127.0.0.1:8096";
-    locations."/" = {};
+    locations = {
+      "/".custom = {
+        extraConfig = ''
+          proxy_buffering off;
+        '';
+      };
+      "/socket".custom = {
+        proxyWebsockets = true;
+      };
+    };
     custom = {
       forceSSL = false;
+      extraConfig = ''
+        client_max_body_size 20M;
+      '';
     };
     dashboard = {
       name = "Jellyfin";
