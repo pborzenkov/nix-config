94ed4b04634d4d885228d06dc67758260f3503e8
Pavel Borzenkov <pavel@borzenkov.net>
2024-12-08 15:58:27 +0100

    overlay: fix transmission_4 override

--- overlay.nix
+++ overlay.nix
@@ -17,7 +17,7 @@ final: prev: {
 
   p1-exporter = final.callPackage ./packages/p1-exporter {};
 
-  transmission = prev.transmission.overrideAttrs (old: rec {
+  transmission_4 = prev.transmission_4.overrideAttrs (old: rec {
     version = "4.0.5";
     src = final.fetchFromGitHub {
       owner = "transmission";
