215238cbb5925c6d0452a583a85834ff0b4b5e1b
Pavel Borzenkov <pavel@borzenkov.net>
2025-01-04 13:48:51 +0100

    metal: allow user to access Keychron K7 Max

--- machines/metal/configs/keyboard.nix
+++ machines/metal/configs/keyboard.nix
@@ -3,7 +3,10 @@
     (pkgs.writeTextFile {
       name = "keychron-k7";
       text = ''
+        # Keychron K7 Pro
         KERNEL=="hidraw*", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0270", TAG+="uaccess"
+        # Keychron K7 Max
+        KERNEL=="hidraw*", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0a70", TAG+="uaccess"
       '';
       destination = "/etc/udev/rules.d/70-keychron-k7.rules";
     })
