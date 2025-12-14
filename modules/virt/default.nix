67f706b4dd33d3a2d11eca1ee9ee8e75751e9ee2
Pavel Borzenkov <pavel@borzenkov.net>
2025-12-14 15:49:48 +0100

    virt: add subuid/subgid for rootless podman

--- modules/virt/default.nix
+++ modules/virt/default.nix
@@ -16,6 +16,21 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    # TODO: for rootless podman (https://github.com/nikstur/userborn/issues/7)
+    environment.etc =
+      let
+        autosubs = lib.pipe config.users.users [
+          lib.attrValues
+          (lib.filter (u: u.uid != null && u.isNormalUser))
+          (lib.concatMapStrings (u: "${toString u.uid}:${toString (100000 + u.uid * 65536)}:65536\n"))
+        ];
+      in
+      {
+        "subuid".text = autosubs;
+        "subuid".mode = "0444";
+        "subgid".text = autosubs;
+        "subgid".mode = "0444";
+      };
     virtualisation = {
       libvirtd = {
         enable = true;
